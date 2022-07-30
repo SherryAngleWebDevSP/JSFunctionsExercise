545a0418d238d96b5c5d0655792364990586b819
SherryAngle <sherryangleWD@gmail.com>
2022-07-30 11:36:15 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//Exercise 1: Print Odds
+function printOdds(count) {
+    for(let i = 1; i <= Math.abs(count); i++) {
+        
+    }
+}
+
+//Exercise 2: Legal
+
+//Exercise 3: Which Quadrant
+
+//Exercise 4: What Type of Triangle
+
+//Exercise 5: Data Plan Status
+
